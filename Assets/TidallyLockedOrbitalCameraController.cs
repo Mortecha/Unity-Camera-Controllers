8c3ed33486ddb919593f0d3d7f976a205872a2d0
Mortecha <sam.keightley@gmail.com>
2019-12-25 23:24:01 +1000

    Initial Tidally Locked Orbital Camera Controller

---
+++ Assets/TidallyLockedOrbitalCameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TidallyLockedOrbitalCameraController : MonoBehaviour
+{
+    public GameObject target;
+    public float rotationSpeed = 8.0f;
+    public float cameraZoom = 10.0f;
+    private float xRotation = 0;
+    private float yRotation = 0;
+
+    void Update()
+    {
+        xRotation += Input.GetAxis("Mouse X") * rotationSpeed;
+        yRotation += Input.GetAxis("Mouse Y") * rotationSpeed;
+
+        transform.rotation = Quaternion.Euler(0, xRotation, 0) * Quaternion.Euler(-yRotation, 0, 0);
+        transform.position = target.transform.position - transform.forward * cameraZoom;
+    }
+}
