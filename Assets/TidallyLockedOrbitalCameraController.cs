4fecd61700f9e25879eb9281fdfeb8c3c9bcf2d3
Mortecha <sam.keightley@gmail.com>
2019-12-26 17:13:06 +1000

    Added mouse drag rotation to TLOCC

--- Assets/TidallyLockedOrbitalCameraController.cs
+++ Assets/TidallyLockedOrbitalCameraController.cs
@@ -7,15 +7,24 @@ public class TidallyLockedOrbitalCameraController : MonoBehaviour
     public GameObject target;
     public float rotationSpeed = 8.0f;
     public float cameraZoom = 10.0f;
+
     private float xRotation = 0;
     private float yRotation = 0;
 
+    void Start()
+    {
+        transform.position = target.transform.position - transform.forward * cameraZoom;
+    }
+
     void Update()
     {
-        xRotation += Input.GetAxis("Mouse X") * rotationSpeed;
-        yRotation += Input.GetAxis("Mouse Y") * rotationSpeed;
+        if(Input.GetMouseButton(0))
+        {
+            xRotation += Input.GetAxis("Mouse X") * rotationSpeed;
+            yRotation += Input.GetAxis("Mouse Y") * rotationSpeed;
 
-        transform.rotation = Quaternion.Euler(0, xRotation, 0) * Quaternion.Euler(-yRotation, 0, 0);
-        transform.position = target.transform.position - transform.forward * cameraZoom;
+            transform.rotation = Quaternion.Euler(0, xRotation, 0) * Quaternion.Euler(-yRotation, 0, 0);
+            transform.position = target.transform.position - transform.forward * cameraZoom;
+        }
     }
 }
