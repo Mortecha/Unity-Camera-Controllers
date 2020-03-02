c611fc9599d289cabf2a927228778022574c9135
Mortecha <sam.keightley@gmail.com>
2020-03-02 13:44:36 +1000

    Simplified controller

--- Assets/Scripts/FlythroughCameraController.cs
+++ Assets/Scripts/FlythroughCameraController.cs
@@ -4,47 +4,39 @@ using UnityEngine;
 
 public class FlythroughCameraController : MonoBehaviour
 {
-    public float movementSpeed = 0.5f;
-    public float rotationSpeed = 720.0f;
+    public float movementSpeed = 50.0f;
+    public float rotationSpeed = 50;
     public float lerpSpeed = 20.0f;
 
-    private Vector3 currentPosition;
+    private float rotationX = 0.0f;
+    private float rotationY = 0.0f;
+
     private Vector3 targetPosition;
 
     void Start()
     {
-        currentPosition = targetPosition = transform.position;
+        Screen.lockCursor = true;
+        targetPosition = transform.position;
     }
 
     void Update()
     {
-        currentPosition = transform.position;
-        if (Input.GetKey(KeyCode.W))
-            targetPosition += transform.forward * movementSpeed;
-        else if (Input.GetKey(KeyCode.S))
-            targetPosition -= transform.forward * movementSpeed;
+        Move();
+        Rotate();
+    }
 
-        if (Input.GetKey(KeyCode.A))
-            targetPosition -= transform.right * movementSpeed;
-        else if (Input.GetKey(KeyCode.D))
-            targetPosition += transform.right * movementSpeed;
+    private void Move()
+    {
+        targetPosition += transform.forward * movementSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
+        targetPosition += transform.right * movementSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
+        transform.position = Vector3.Lerp(transform.position, targetPosition, lerpSpeed * Time.deltaTime);
+    }
 
-        if (Input.GetKey(KeyCode.Q))
-            targetPosition -= transform.up * movementSpeed;
-        else if (Input.GetKey(KeyCode.E))
-            targetPosition += transform.up * movementSpeed;
-
-        transform.position = Vector3.Lerp(currentPosition, targetPosition, lerpSpeed * Time.deltaTime);
-
-        if (Input.GetMouseButton(1) || Input.GetMouseButton(2))
-        {
-            float x = Input.GetAxis("Mouse X") * Time.deltaTime * 50 * rotationSpeed * Mathf.Deg2Rad;
-            float y = Input.GetAxis("Mouse Y") * Time.deltaTime * 50 * rotationSpeed * Mathf.Deg2Rad;
-
-            Vector3 localEulerAngles = transform.localEulerAngles;
-            localEulerAngles.x -= y;
-            localEulerAngles.y += x;
-            transform.localEulerAngles = localEulerAngles;
-        }
+    private void Rotate()
+    {
+        rotationX += Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
+        rotationY += Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime;
+        transform.localRotation = Quaternion.AngleAxis(rotationX, Vector3.up);
+        transform.localRotation *= Quaternion.AngleAxis(rotationY, Vector3.left);
     }
 }
